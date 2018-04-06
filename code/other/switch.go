4c435f53ecd70d88ab7acfea356cd832d1879f5e
tcsecchen <553010433@qq.com>
2018-04-07 02:37:58 +0800

    Revert "配置环境"
    This reverts commit 9cf0fe00c6551e47f6d5f82c3bbf8d093ef18777.

--- code/other/switch.go
+++ code/other/switch.go
@@ -5,26 +5,26 @@ import (
 	"os"
 )
 
-func mySwitch() {
-	a, b, c := 0, 0, 0
-	for _, opcase := range os.Args[1:] {
-		switch opcase {
-		case "1":
-			a = 1
-			fmt.Println("1")
-		case "2":
-			b = 2
-			fmt.Println("2")
-		case "3":
-			c = 3
-			fmt.Println("3")
-		default:
-			a, b, c = 0, 0, 0
-			fmt.Println("default")
+func main(){
+	a, b, c := 0, 0, 0;
+	for _, opcase := range os.Args[1:]{
+		switch opcase{
+			case "1":
+				a = 1;
+				fmt.Println("1");
+			case "2":
+				b = 2;
+				fmt.Println("2");
+			case "3":
+				c = 3;
+				fmt.Println("3");
+			default:
+				a, b, c = 0, 0, 0;
+				fmt.Println("default");	
 		}
 	}
-	println(a, b, c)
+	println(a,b,c);
 	//交换
-	a, b = b, a
-	println(a, b, c)
+	a ,b = b, a;
+	println(a,b,c);
 }
--- code/other/指针数组.go
+++ code/other/指针数组.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 )
+const MAX int = 3
 
-const max int = 3
-
-func main() {
-	a := []int{10, 100, 200}
+func main(){
+	a := []int{10,100,200}
 	var i int
-	var ptr [max]*int
-
-	for i = 0; i < max; i++ {
-		ptr[i] = &a[i] /* 整数地址赋值给指针数组 */
+	var ptr [MAX]*int;
+ 
+	for  i = 0; i < MAX; i++ {
+	   ptr[i] = &a[i] /* 整数地址赋值给指针数组 */
 	}
-
-	for i = 0; i < max; i++ {
-		fmt.Printf("a[%d] = %d\n", i, *ptr[i])
+ 
+	for  i = 0; i < MAX; i++ {
+	   fmt.Printf("a[%d] = %d\n", i,*ptr[i] )
 	}
 }
