9cf0fe00c6551e47f6d5f82c3bbf8d093ef18777
tcsecchen <553010433@qq.com>
2018-04-07 02:20:27 +0800

    配置环境

--- code/other/指针数组.go
+++ code/other/指针数组.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 )
-const MAX int = 3
 
-func main(){
-	a := []int{10,100,200}
+const max int = 3
+
+func main() {
+	a := []int{10, 100, 200}
 	var i int
-	var ptr [MAX]*int;
- 
-	for  i = 0; i < MAX; i++ {
-	   ptr[i] = &a[i] /* 整数地址赋值给指针数组 */
+	var ptr [max]*int
+
+	for i = 0; i < max; i++ {
+		ptr[i] = &a[i] /* 整数地址赋值给指针数组 */
 	}
- 
-	for  i = 0; i < MAX; i++ {
-	   fmt.Printf("a[%d] = %d\n", i,*ptr[i] )
+
+	for i = 0; i < max; i++ {
+		fmt.Printf("a[%d] = %d\n", i, *ptr[i])
 	}
 }
