f6cd4bbaa578447f4c3803834b3f9c0fc5c728f6
tcsecchen <553010433@qq.com>
2018-04-23 00:34:00 +0800

    mapinterface

---
+++ code/interface/mapInterface/mapInterface.go
@@ -0,0 +1,23 @@
+//遍历 map[string]interface{} 类型的value值
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	txt := `{"a":1,"b":2,"c":[{"name":"1","group":"2"},{"name":"3","group":"4"}]}`
+	var m map[string]interface{}
+	if err := json.Unmarshal([]byte(txt), &m); err != nil {
+		panic(err)
+	}
+	for i, v := range m {
+		fmt.Printf("%v : %v \n", i, v)
+	}
+	//interface 类型转换
+	//value.(int)转为int，value.(string) 转为 string
+	for _, v := range m["c"].([]interface{}) {
+		fmt.Println(v)
+	}
+}
